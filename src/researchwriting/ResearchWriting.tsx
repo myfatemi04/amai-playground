8c799325b45aae3c67d3438868345960192ebefc
Michael Fatemi <myfatemi04@gmail.com>
2022-12-11 13:49:22 -0500

    smart completions

--- src/researchwriting/ResearchWriting.tsx
+++ src/researchwriting/ResearchWriting.tsx
@@ -1,10 +1,12 @@
-import { CSSProperties, useCallback, useRef, useState } from "react";
+import React, { CSSProperties, useCallback, useRef, useState } from "react";
 import { api } from "../api";
 
 interface Suggestion {
   text: string;
   position: number;
 }
+
+const SUGGESTION_DELAY_MS = 2000;
 
 /*
 Creates a word processor which is augmented with AI problem-solving tools.
@@ -13,27 +15,60 @@ export default function ResearchWriting() {
   const [content, setContent] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [suggestion, setSuggestion] = useState<Suggestion | null>(null);
+  const createSuggestionTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [generationStatus, setGenerationStatus] = useState<
+    null | "errored" | "pending"
+  >(null);
+
+  const createSuggestion = useCallback(async () => {
+    if (!textareaRef.current) {
+      return;
+    }
+    setGenerationStatus("pending");
+    const textarea = textareaRef.current;
+    const selection = {
+      selectionStart: textarea.selectionStart,
+      selectionEnd: textarea.selectionEnd,
+    };
+    const before = textarea.value.slice(0, selection.selectionEnd);
+    // const after = textarea.value.slice(selection.selectionEnd);
+    try {
+      const result = await api("generate_completion", { prompt: before });
+      if (result.completion) {
+        setSuggestion({
+          text: result.completion,
+          position: selection.selectionEnd,
+        });
+        setGenerationStatus(null);
+      }
+    } catch (e) {
+      console.error(e);
+      setGenerationStatus("errored");
+    }
+  }, []);
+
+  const onTextareaChange = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setContent(e.target.value);
+      if (createSuggestionTimeoutRef.current) {
+        clearTimeout(createSuggestionTimeoutRef.current);
+      }
+      createSuggestionTimeoutRef.current = setTimeout(
+        createSuggestion,
+        SUGGESTION_DELAY_MS
+      );
+    },
+    [createSuggestion]
+  );
 
   const onTextareaKeyDown = useCallback(
-    async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
       if (!textareaRef.current) {
         return;
       }
       const textarea = textareaRef.current;
       if (e.code === "Enter" && e.ctrlKey) {
-        const selection = {
-          selectionStart: textarea.selectionStart,
-          selectionEnd: textarea.selectionEnd,
-        };
-        const before = textarea.value.slice(0, selection.selectionEnd);
-        // const after = textarea.value.slice(selection.selectionEnd);
-        const result = await api("generate_completion", { prompt: before });
-        if (result.completion) {
-          setSuggestion({
-            text: result.completion,
-            position: selection.selectionEnd,
-          });
-        }
+        createSuggestion();
       } else if ((e.code === "Tab" || e.code === "ArrowRight") && suggestion) {
         e.preventDefault();
         const before = textarea.value.slice(0, suggestion.position);
@@ -45,7 +80,7 @@ export default function ResearchWriting() {
         setSuggestion(null);
       }
     },
-    [suggestion]
+    [createSuggestion, suggestion]
   );
 
   const textareaStyle: CSSProperties = {
@@ -79,6 +114,13 @@ export default function ResearchWriting() {
           }}
         >
           <h3>Writing Panel</h3>
+          <span style={{ textTransform: "uppercase", fontSize: "0.75rem" }}>
+            {generationStatus &&
+              {
+                errored: "Error",
+                pending: "Generating...",
+              }[generationStatus]}
+          </span>
           <div style={{ position: "relative", height: "100%" }}>
             <div
               style={{
@@ -96,6 +138,7 @@ export default function ResearchWriting() {
                       margin: 0,
                       fontFamily: textareaStyle.fontFamily,
                       display: "inline",
+                      whiteSpace: "pre-wrap",
                     }}
                   >
                     {content.slice(0, suggestion.position)}
@@ -107,6 +150,7 @@ export default function ResearchWriting() {
                       margin: 0,
                       fontFamily: textareaStyle.fontFamily,
                       display: "inline",
+                      whiteSpace: "pre-wrap",
                     }}
                   >
                     {suggestion.text}
@@ -116,7 +160,7 @@ export default function ResearchWriting() {
             </div>
             <textarea
               ref={textareaRef}
-              onChange={(e) => setContent(e.target.value)}
+              onChange={onTextareaChange}
               onKeyDown={onTextareaKeyDown}
               value={content}
               rows={40}
