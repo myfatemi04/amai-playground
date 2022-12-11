54e5b358f7125beb0e217fdf4e2405eb7584c61f
Michael Fatemi <myfatemi04@gmail.com>
2022-12-11 16:04:46 -0500

    incorporate search results

--- src/researchwriting/ResearchWriting.tsx
+++ src/researchwriting/ResearchWriting.tsx
@@ -1,12 +1,122 @@
-import React, { CSSProperties, useCallback, useRef, useState } from "react";
+import { Readability } from "@mozilla/readability";
+import React, {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { api } from "../api";
 
-interface Suggestion {
-  text: string;
-  position: number;
+interface SearchResultsSuggestionCache {
+  [url: string]:
+    | {
+        parseable: true;
+        pageContent: string;
+        completion: string;
+      }
+    | {
+        parseable: false;
+        pageContent: null;
+        completion: null;
+      };
+}
+
+type Suggestion =
+  | {
+      type: "text";
+      text: string;
+      position: number;
+    }
+  | {
+      type: "searchResults";
+      results: SearchResults;
+      cache: SearchResultsSuggestionCache;
+      position: number;
+      question: string;
+    }
+  | {
+      type: "none";
+      position: number;
+    };
+
+interface SearchResultPage {
+  title: string;
+  url: string;
+  snippet: string;
+}
+
+interface SearchResults {
+  pages: SearchResultPage[];
+  related_searches: string[];
+  query: string;
 }
 
 const SUGGESTION_DELAY_MS = 2000;
+
+async function getCompletionBasedOnSearchResult(
+  knowledge: string,
+  context: string
+) {
+  const { completion } = await api("generate_for_prompt", {
+    prompt_id: "639639a9fe3937783e18ce52",
+    variables: { knowledge, context },
+  });
+  return completion as string;
+}
+
+async function getPageHtml(url: string) {
+  const { result } = await api("retrieval_enhancement", {
+    backend: "proxy",
+    query: url,
+  });
+  return result as string;
+}
+
+function parse(html: string) {
+  const document = new DOMParser().parseFromString(html, "text/html");
+  const reader = new Readability(document);
+  const article = reader.parse();
+  return article;
+}
+
+async function getSearchQueries(question: string): Promise<string[]> {
+  const { completion } = await api("generate_for_prompt", {
+    prompt_id: "63962e8cfe3937783e18ce51",
+    variables: { question },
+  });
+  return completion.map((completion: string) => {
+    if (completion.startsWith(`"`) && completion.endsWith(`"`)) {
+      return completion.slice(1, completion.length - 1);
+    } else {
+      return completion;
+    }
+  });
+}
+
+async function getSearchResults(query: string): Promise<SearchResults> {
+  const { result } = await api("retrieval_enhancement", {
+    backend: "bing",
+    query,
+  });
+  return { ...result, query };
+}
+
+async function getKnowledgeFromPage(
+  title: string,
+  content: string,
+  question: string
+) {
+  const { completion } = await api("generate_for_prompt", {
+    prompt_id: "639641b7fe3937783e18ce53",
+    variables: {
+      title,
+      content,
+      question,
+    },
+  });
+  return completion;
+}
 
 /*
 Creates a word processor which is augmented with AI problem-solving tools.
@@ -20,6 +130,18 @@ export default function ResearchWriting() {
   const [generationStatus, setGenerationStatus] = useState<
     null | "errored" | "pending"
   >(null);
+
+  useEffect(() => {
+    getPageHtml("https://en.wikipedia.org/wiki/Orange_juice").then((html) => {
+      const article = parse(html);
+      if (article) {
+        const content = article.textContent.replace(/[\r\n]{3+}/g, "\n\n");
+        console.log(content);
+      } else {
+        console.log("Article could not be parsed");
+      }
+    });
+  }, []);
 
   const createSuggestion = useCallback(async () => {
     if (!textareaRef.current) {
@@ -35,28 +157,120 @@ export default function ResearchWriting() {
     if (previousSuggestionContextRef.current?.startsWith(before)) {
       return;
     }
+    // If the textarea ends with "([question]?)", then generate search queries.
     setGenerationStatus("pending");
     try {
+      if (before.endsWith("?)")) {
+        const question = before.slice(
+          before.lastIndexOf("(") + 1,
+          before.length - 1
+        );
+        const queries = await getSearchQueries(question);
+        const results = await getSearchResults(queries[0]);
+        setSuggestion({
+          type: "searchResults",
+          results,
+          position: selection.selectionEnd,
+          cache: {},
+          question,
+        });
+        setGenerationStatus(null);
+        return;
+      }
+
       const result = await api("generate_completion", { prompt: before });
       if (result.completion) {
         setSuggestion({
+          type: "text",
           text: result.completion,
           position: selection.selectionEnd,
         });
         setGenerationStatus(null);
         previousSuggestionContextRef.current = before;
       } else {
-				setSuggestion({
-          text: "(no suggestion)",
+        setSuggestion({
+          type: "none",
           position: selection.selectionEnd,
         });
-				setGenerationStatus(null);
-			}
+        setGenerationStatus(null);
+      }
     } catch (e) {
       console.error(e);
       setGenerationStatus("errored");
     }
   }, []);
+
+  const finishSuggestionWithSearchResult = useCallback(
+    async (url: string) => {
+      if (!suggestion) {
+        console.error("No suggestion to finish");
+        return;
+      }
+      if (suggestion.type !== "searchResults") {
+        console.error("Suggestion is not based on search results");
+        return;
+      }
+      const pageResult = suggestion.results.pages.find(
+        (page) => page.url === url
+      );
+      if (!pageResult) {
+        console.error("Page not found in search results");
+        return;
+      }
+      if (!(url in suggestion.cache)) {
+        let pageContent: string | null;
+        try {
+          pageContent = parse(await getPageHtml(url))?.textContent ?? null;
+        } catch (e) {
+          console.error(e);
+          pageContent = null;
+        }
+        if (pageContent === null) {
+          suggestion.cache[url] = {
+            parseable: false,
+            pageContent: null,
+            completion: null,
+          };
+        } else {
+          const knowledge = await getKnowledgeFromPage(
+            pageResult.title,
+            pageContent,
+            suggestion.question
+          );
+          const contextWithQuestion = content.slice(0, suggestion.position);
+          const contextWithoutQuestion = contextWithQuestion.slice(
+            0,
+            contextWithQuestion.lastIndexOf("(")
+          );
+          suggestion.cache[url] = {
+            parseable: true,
+            pageContent,
+            completion: await getCompletionBasedOnSearchResult(
+              knowledge,
+              contextWithoutQuestion
+            ),
+          };
+        }
+      }
+
+      const completionForURL = suggestion.cache[url]!;
+
+      if (!completionForURL.parseable) {
+        setSuggestion({
+          type: "none",
+          position: suggestion.position,
+        });
+        return;
+      } else {
+        setSuggestion({
+          type: "text",
+          text: completionForURL.completion,
+          position: suggestion.position,
+        });
+      }
+    },
+    [content, suggestion]
+  );
 
   const onTextareaChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -80,7 +294,10 @@ export default function ResearchWriting() {
       const textarea = textareaRef.current;
       if (e.code === "Enter" && e.ctrlKey) {
         createSuggestion();
-      } else if ((e.code === "Tab" || e.code === "ArrowRight") && suggestion) {
+      } else if (
+        (e.code === "Tab" || e.code === "ArrowRight") &&
+        suggestion?.type === "text"
+      ) {
         e.preventDefault();
         const before = textarea.value.slice(0, suggestion.position);
         const after = textarea.value.slice(suggestion.position);
@@ -115,7 +332,11 @@ export default function ResearchWriting() {
       }}
     >
       <h1>Research Writing Assistant</h1>
-      <div style={{ display: "flex", height: "100%" }}>
+      {/*
+				minHeight: 0 might not seem to make sense here, but by default, it's minHeight: auto. This causes overflows of the flex box.
+				More info can be found here: https://stackoverflow.com/questions/36230944/prevent-flex-items-from-overflowing-a-container
+			*/}
+      <div style={{ display: "flex", minHeight: 0, flexGrow: 1 }}>
         <div
           style={{
             flex: 4,
@@ -138,7 +359,7 @@ export default function ResearchWriting() {
                 pending: "Generating...",
               }[generationStatus]}
           </span>
-          <div style={{ position: "relative", height: "100%" }}>
+          <div style={{ position: "relative", height: "100%", flexGrow: 1 }}>
             <div
               style={{
                 ...textareaStyle,
@@ -170,7 +391,14 @@ export default function ResearchWriting() {
                       whiteSpace: "pre-wrap",
                     }}
                   >
-                    {suggestion.text}
+                    {
+                      {
+                        // @ts-ignore
+                        text: suggestion.text,
+                        none: "(no suggestion)",
+                        searchResults: "(search results)",
+                      }[suggestion.type]
+                    }
                   </pre>
                 </>
               )}
@@ -187,7 +415,39 @@ export default function ResearchWriting() {
           </div>
         </div>
         <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <h3>Research Panel</h3>
+          <h3 style={{ margin: "0.5rem 0" }}>Research Panel</h3>
+          {/* <input
+            type="text"
+            onChange={(e) => setResearchPanelSearchText(e.target.value)}
+            value={researchPanelSearchText}
+          /> */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              overflowY: "auto",
+              paddingRight: "1rem",
+              margin: "1rem 0",
+              boxSizing: "border-box",
+              userSelect: "none",
+            }}
+          >
+            {suggestion?.type === "searchResults" &&
+              suggestion.results.pages.map((result) => (
+                <div
+                  key={result.url}
+                  style={{ cursor: "pointer" }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    finishSuggestionWithSearchResult(result.url);
+                  }}
+                >
+                  <b>{result.title}</b>
+                  <p>{result.snippet}</p>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
