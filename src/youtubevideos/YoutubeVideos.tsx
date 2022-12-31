e61fe950c0adc4a4af3c5d99445c6b3424a86604
Michael Fatemi <myfatemi04@gmail.com>
2022-12-31 17:21:43 -0500

    clear when URL changes

--- src/youtubevideos/YoutubeVideos.tsx
+++ src/youtubevideos/YoutubeVideos.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { getYoutubeTranscript, TranscriptItem } from "../api";
 import Button from "../Button";
 import Header from "../Header";
@@ -58,6 +58,11 @@ export default function YoutubeVideos() {
         setStatus(`Error: ${s}`);
       }
     }
+  }, [url]);
+  useEffect(() => {
+    setDisplay(false);
+    setStatus("Ready");
+    setTranscript(null);
   }, [url]);
 
   return (
